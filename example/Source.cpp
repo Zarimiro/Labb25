f2a16ab889ea7fa072f0e443483b818ee4336bc2
Zarimiro <vadimzarya@yandex.ru>
2017-04-28 01:35:28 +0400

    Create Source.cpp

---
+++ example/Source.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+#include<trees.h>
+
+int main() {
+	BinarySearchTree <int> *Tree = NULL;
+	add_branch(5, Tree);
+	add_branch(4, Tree);
+	add_branch(6, Tree);
+	add_branch(3, Tree);
+	add_branch(6, Tree);
+	add_branch(7, Tree);
+	add_branch(8, Tree);
+	print_all(Tree);
+
+	search_all(Tree, 6);
+	search_all(Tree, 8);
+
+
+	save(Tree);
+
+	clear(Tree);
+	_getch();
+	return 0;
+}
