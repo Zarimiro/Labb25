7c4ce92b28f4a72ec7c4bbdd3313421736420289
Zarimiro <vadimzarya@yandex.ru>
2017-04-28 10:17:35 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,2 +1,53 @@
 #include<Labb25.hpp>
 #include<catch.hpp>
+
+SCENARIO("Inserting a nonexistent node") {
+    BinarySearchTree <int> *Tree=NULL;
+    add_branch(5, Tree);
+    int rv = add_branch(6, Tree);;
+    
+    REQUIRE( rv == 0);
+}
+
+SCENARIO("Inserting an already existent node") {
+    BinarySearchTree <int> *Tree=NULL;
+    add_branch(5, Tree);
+    int rv = add_branch(5, Tree);;
+    
+    REQUIRE( rv == 1);
+}
+SCENARIO("Search for an existent node") {
+   BinarySearchTree <int> *Tree=NULL;
+    add_branch(5, Tree);
+    add_branch(6, Tree);
+    int rv = search_all(Tree,6);
+    
+    REQUIRE( rv == 0);
+}
+
+SCENARIO("Search for a nonexistent node") {
+   BinarySearchTree <int> *Tree=NULL;
+    add_branch(5, Tree);
+    add_branch(6, Tree);
+    int rv = search_all(Tree,7);
+    
+    REQUIRE( rv == 1);
+}
+
+SCENARIO("Deleting an existent node") {
+    binarytree <int> tree;
+    tree.insert(14);
+    tree.insert(15);
+    int rv = tree.destroynode(15);
+    
+    REQUIRE( rv == 0);
+}
+
+SCENARIO("Deleting a nonexistent node") {
+    binarytree <int> tree;
+    tree.insert(14);
+    tree.insert(15);
+    int rv = tree.destroynode(17);
+    
+    REQUIRE( rv == 1);
+}
